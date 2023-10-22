8603c2514b6bdef5438dde6af34831cc2f168d73
SuneelKumar Vanapalli <suneel.vanapalli@adudev.com>
2023-10-22 12:13:44 +1100

    moved to folder

--- react-redux-practice-app/src/components/NewTodo/NewTodo.tsx
+++ react-redux-practice-app/src/components/NewTodo/NewTodo.tsx
@@ -1,24 +1,35 @@
-import { useRef, useContext } from 'react';
+import { useRef, useContext, useState } from 'react';
 import { Todo } from '../../models/todo';
 import classes from './NewTodo.module.css';
 import { TodosContext } from '../../context/TodoContext';
 import Button from '../../shared/Button';
 import Text from '../../shared/Text';
+import React from 'react';
 
 //const NewTodo: React.FC<{ onAddTodo: (newTodo: Todo) => void }> = (props) => {
 const NewTodo: React.FC = () => {
+  const [isValid, setIsValid] = useState(true);
   const todoContext = useContext(TodosContext);
   const submitHander = (event: React.FormEvent) => {
     event.preventDefault();
     const todoId = idRef.current!.value;
     const todoValue = nameRef.current!.value;
 
+    if (todoId.trim().length === 0 && todoValue.trim().length === 0) {
+      setIsValid(() => {
+        return false;
+      });
+      return;
+    }
     const newTodo = new Todo(todoId, todoValue);
     todoContext.onAddNewTodo(newTodo);
+    setIsValid(() => {
+      return true;
+    });
   };
 
-  const idRef = useRef<HTMLInputElement>(null);
-  const nameRef = useRef<HTMLInputElement>(null);
+  const idRef = React.createRef<HTMLInputElement>();
+  const nameRef = React.createRef<HTMLInputElement>();
 
   return (
     <form className={classes.form} onSubmit={submitHander}>
---
+++ react-redux-practice-app/src/shared/Text.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import classes from './Text.module.css';
+
+// import styled from 'styled-components';
+
+// const Text = styled.input.attrs({ type: 'text' }) `
+//     display: block;
+//     width: 100%;
+//     font: inherit;
+//     font-size: 1.5rem;
+//     padding: 0.5rem;
+//     border-radius: 4px;
+//     background-color: #f7f5ef;
+//     border: none;
+//     border-bottom: 2px solid #494844;
+//     border-bottom-right-radius: 0;
+//     border-bottom-left-radius: 0;
+//     margin-bottom: 0.5rem;
+// `;
+type ButtonProps = React.HTMLProps<HTMLButtonElement>
+
+const Text =  React.forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => (
+    <button
+      type={props.type}
+      onClick={props.onClick}
+      className={classes.button}
+    >
+      props.children
+    </button>
+};
+
+export default Text;
