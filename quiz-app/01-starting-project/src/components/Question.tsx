16e2baa82beaceb74647fdc85e092de12aecf4f3
Suneel Kumar Vanapalli <suneel.vanapalli@gmail.com>
2024-06-12 22:21:27 +1000

    refactor

--- quiz-app/01-starting-project/src/components/Question.tsx
+++ quiz-app/01-starting-project/src/components/Question.tsx
@@ -1,27 +1,56 @@
 import Answers from './Answers';
 import QuestionTimer from './QuestionTimer';
+import QUESTIONS from '../assets/questions';
+import { useState } from 'react';
 
 export default function Question({
-  questionText,
-  answers,
-  activeQuestionIndex,
-  answerState,
-  selectedAnswer,
+  index,
   onSelectAnswer,
   onNullAnswerSelection,
 }) {
+  const [answer, setAnswer] = useState<{
+    selectedAnswer: string;
+    isCorrect: boolean | null;
+  }>({
+    selectedAnswer: '',
+    isCorrect: null,
+  });
+
+  function handleSelectedAnswer(answer) {
+    setAnswer({
+      selectedAnswer: answer,
+      isCorrect: null,
+    });
+    setTimeout(() => {
+      setAnswer({
+        selectedAnswer: answer,
+        isCorrect: answer === QUESTIONS[index].answers[0],
+      });
+    }, 2000);
+    onSelectAnswer(answer);
+  }
+
+  let answerState = '';
+  if (answer.selectedAnswer) {
+    answerState = 'answered';
+    if (answer.isCorrect) {
+      answerState = 'correct';
+    } else {
+      answerState = 'wrong';
+    }
+  }
   return (
     <div id='question'>
       <QuestionTimer
         timeout={10000}
         onTimeOutComplete={onNullAnswerSelection}
       ></QuestionTimer>
-      <h2>{questionText}</h2>
+      <h2>{QUESTIONS[index].text}</h2>
       <Answers
-        answers={answers}
+        answers={QUESTIONS[index].answers}
         answerState={answerState}
-        selectedAnswer={selectedAnswer}
-        onSelect={onSelectAnswer}
+        selectedAnswer={answer}
+        onSelect={handleSelectedAnswer}
       ></Answers>
     </div>
   );
--- quiz-app/01-starting-project/src/components/Quiz.tsx
+++ quiz-app/01-starting-project/src/components/Quiz.tsx
@@ -1,37 +1,34 @@
 import { useCallback, useEffect, useRef, useState } from 'react';
 import questions from '../assets/questions';
 import quizCompletedImg from '../assets/quiz-complete.png';
-import QuestionTimer from './QuestionTimer';
-import Answers from './Answers';
 import Question from './Question';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const [answerState, setAnswerState] = useState('');
+  // const [answerState, setAnswerState] = useState('');
 
-  let activeQuestionIndex =
-    answerState === '' ? userAnswers.length : userAnswers.length - 1;
+  let activeQuestionIndex = userAnswers.length;
 
   const quizIsComplete = activeQuestionIndex === questions.length;
 
   let handleUserAnswer = useCallback(
     function handleUserAnswer(selectedAnswer) {
-      setAnswerState('selected');
+      //setAnswerState('selected');
       setUserAnswers((prevState) => {
         return [...prevState, selectedAnswer];
       });
 
-      setTimeout(() => {
-        if (selectedAnswer === questions[activeQuestionIndex].answers[0]) {
-          setAnswerState('correct');
-        } else {
-          setAnswerState('wrong');
-        }
+      // setTimeout(() => {
+      //   if (selectedAnswer === questions[activeQuestionIndex].answers[0]) {
+      //     setAnswerState('correct');
+      //   } else {
+      //     setAnswerState('wrong');
+      //   }
 
-        setTimeout(() => {
-          setAnswerState('');
-        }, 2000);
-      }, 2000);
+      //   setTimeout(() => {
+      //     setAnswerState('');
+      //   }, 2000);
+      // }, 2000);
     },
     [activeQuestionIndex]
   );
@@ -53,11 +50,7 @@ export default function Quiz() {
     <div id='quiz'>
       <Question
         key={activeQuestionIndex}
-        activeQuestionIndex={activeQuestionIndex}
-        answerState={answerState}
-        selectedAnswer={userAnswers[userAnswers.length - 1]}
-        questionText={questions[activeQuestionIndex].text}
-        answers={questions[activeQuestionIndex].answers}
+        index={activeQuestionIndex}
         onSelectAnswer={handleUserAnswer}
         onNullAnswerSelection={handleNullAnswerSelection}
       ></Question>
