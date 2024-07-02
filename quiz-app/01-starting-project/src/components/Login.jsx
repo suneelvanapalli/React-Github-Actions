bcfe711ef661e1a31207e0644f3e3e6aec2db7f5
Suneel Kumar Vanapalli <suneel.vanapalli@gmail.com>
2024-07-02 20:52:38 +1000

    save form state

--- quiz-app/01-starting-project/src/components/Login.jsx
+++ quiz-app/01-starting-project/src/components/Login.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 export default function Login() {
   const handleSubmit = (event) => { 
     event.preventDefault();
-    console.log('submitted');
+    console.log(formState);
   }
+
+  const [formState, setFormState] = useState({ email: '', password: '' });
+
+  const saveFormState = (identifier, value) => {
+    setFormState((prevState) => {
+      return {
+        ...prevState,
+        [identifier]: value
+      };
+    });
+  }
+
   return (
     <form onSubmit={handleSubmit}>
       <h2>Login</h2>
@@ -10,12 +24,12 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email" onChange={(event) => { saveFormState('email', event.target.value) }} />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password" onChange={(event) => { saveFormState('password', event.target.value) }} />
         </div>
       </div>
 
--- quiz-app/01-starting-project/src/main.tsx
+++ quiz-app/01-starting-project/src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.js'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.js';
+import './index.css';
+import './forms.css';
+import Login from './components/Login.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <Login></Login>
+    {/* <App /> */}
+  </React.StrictMode>
+);
