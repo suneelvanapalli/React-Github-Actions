69f5ae3e5319f311618183f1f9b36ad9284e00e0
Suneel Kumar Vanapalli <suneel.vanapalli@gmail.com>
2024-07-02 21:17:01 +1000

    retrieve formdata via Browser's inbuilt FormData

--- quiz-app/01-starting-project/src/main.tsx
+++ quiz-app/01-starting-project/src/main.tsx
@@ -4,10 +4,12 @@ import App from './App.js';
 import './index.css';
 import './forms.css';
 import Login from './components/Login.jsx';
+import Signup from './components/Signup.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Login></Login>
+    <Signup></Signup>
     {/* <App /> */}
   </React.StrictMode>
 );
