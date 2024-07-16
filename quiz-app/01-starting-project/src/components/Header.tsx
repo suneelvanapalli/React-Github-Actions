1a370a9c95b9994d0ed6d5da646756896e94df4c
Suneel Kumar Vanapalli <suneel.vanapalli@gmail.com>
2024-07-16 13:21:16 +1000

    move quiz-app to it's own repo

--- quiz-app/01-starting-project/src/components/Header.tsx
+++
@@ -1,3 +0,0 @@
-export default function Header() {
-  return <p data-testid='header'>test</p>;
-}
--- quiz-app/01-starting-project/src/components/Input.jsx
+++
@@ -1,8 +0,0 @@
-export const Input = ({ id, name, error, innerRef, ...props }) => {
-  return (
-    <>
-      <input id={id} name={name} {...props} ref={innerRef} />
-      <div className='control-error'>{error && <p>{error}</p>}</div>
-    </>
-  );
-};
